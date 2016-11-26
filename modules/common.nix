6cf13d54a5df0dfa575d8f575c9ce2857d6333b8
Brian Olsen <brian@maven-group.org>
2016-11-27 00:56:42 +0100

    remove caches

--- modules/common.nix
+++ modules/common.nix
@@ -22,11 +22,11 @@
 
     # use our hydra builds
     # trustedBinaryCaches = [ "https://cache.nixos.org" "https://hydra.snabb.co" ];
-    trustedBinaryCaches = [ "https://cache.nixos.org" "https://hydra.nixos.org"];
-    binaryCaches = trustedBinaryCaches;
-    binaryCachePublicKeys = [ 
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
-    ];
+    #trustedBinaryCaches = [ "https://cache.nixos.org" "https://hydra.nixos.org"];
+    #binaryCaches = trustedBinaryCaches;
+    #binaryCachePublicKeys = [ 
+    #  "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    #  "hydra.nixos.org-1:CNHJZBh9K4tP3EKF6FkkgeVYsS3ohTl+oS0Qa8bezVs="
+    #];
   };
 }
